25f5d56a482731501b742e1c172f396d99c5276c
Mohammad Rafey <mohammadrafey@Mohammads-MacBook-Pro.local>
2022-03-13 17:39:34 -0400

    INIT: Intial client and server folder/files setup

---
+++ client/src/App.js
@@ -0,0 +1,9 @@
+function App() {
+    return (
+        <div className='App'>
+            <h1>Expense Tracker</h1>
+        </div>
+    )
+}
+
+export default App
---
+++ server/index.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
+const connectDB = require('./config/db')
+
+const app = express()
+
+require('dotenv').config({
+    path: './config/index.env',
+})
+
+connectDB()
+app.use(express.urlencoded({ limit: '50mb', extended: true }))
+app.use(express.json({ limit: '50mb', extended: true }))
+app.use(morgan('dev'))
+app.use(cors())
+
+// Test Route
+app.get('/', (req, res) => {
+    res.send('Test route for Expense Tracker')
+})
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server running on port ${process.env.PORT}`)
+})
