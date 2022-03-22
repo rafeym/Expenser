0be6a8c6d61139491a05cfa3e80a098e4ea2d5ad
Mohammad Rafey <mohammadrafey0@gmail.com>
2022-03-22 18:17:21 -0400

    DEVELOP: created reusable styled components for dashboard components

---
+++ client/src/styles/Reusable.elements.js
@@ -0,0 +1,43 @@
+import { css } from 'styled-components'
+import styled from 'styled-components'
+
+export const cardStyles = css`
+    padding: 2rem 2rem 2rem 2rem;
+    border-radius: 1rem;
+    background-color: white;
+    background: #101522;
+    color: white;
+`
+
+export const Container = styled.div`
+    z-index: 1;
+    width: 100%;
+    max-width: 1300px;
+    margin-right: auto;
+    margin-left: auto;
+    padding-right: 50px;
+    padding-left: 50px;
+    @media screen and (max-width: 991px) {
+        padding-right: 30px;
+        padding-left: 30px;
+    }
+`
+export const Button = styled.button`
+    border-radius: 4px;
+    background: ${({ primary }) => (primary ? '#4b59f7' : '#0467fb')};
+    white-space: nowrap;
+    padding: ${({ big }) => (big ? '12px 64px' : '10px 20px')};
+    color: #fff;
+    font-size: ${({ fontBig }) => (fontBig ? '20px' : '16px')};
+    outline: none;
+    border: none;
+    cursor: pointer;
+    &:hover {
+        transition: all 0.3s ease-out;
+        background: #fff;
+        background: ${({ primary }) => (primary ? '#0467fb' : '#4b59f7')};
+    }
+    @media screen and (max-width: 960px) {
+        width: 100%;
+    }
+`
